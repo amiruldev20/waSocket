890ffc882351f78451e2a9a04b54bfcb13b76e62
amiruldev20 <amiruldev20@gmail.com>
2024-06-23 14:17:20 +0000

    test enc

--- util/cbcutil/cbc_test.go
+++ util/cbcutil/cbc_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestEncryptDecrypt(t *testing.T) {
-	key := []byte("MySecretSecretSecretSecretKey123")
-	plain := []byte("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+	key := []byte("amiruldev1337")
+	plain := []byte("wellcome to waSocket")
 
 	cipher, err := Encrypt(key, nil, plain)
 	if err != nil {
